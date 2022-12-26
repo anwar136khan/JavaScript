938324a22297c4165a740d5b29d64c7938934e10
anwar136khan <117710526+anwar136khan@users.noreply.github.com>
2022-12-26 04:04:21 +0000

    container config done

---
+++ basic/variables.js
@@ -0,0 +1,11 @@
+const accountId=1445553;
+let accountEmail='anwar134@gamil.com';
+var accountPassword='12345';
+accountCity="Meerut";
+
+//accountId=2;
+accountEmail="Hc@hc.com";
+accountPassword="2345";
+accountCity="Noida";
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity]);
