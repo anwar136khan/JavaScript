d78722e284f7b1e972b15638c0d3294212d40cf1
anwar136khan <117710526+anwar136khan@users.noreply.github.com>
2022-12-27 14:54:14 +0000

    dataType

---
+++ basic/dataType.js
@@ -0,0 +1,15 @@
+console.log(3+3)
+
+// number
+// BigInt
+// string
+// boolean
+// null => Standolone value
+// undefined
+// symbol
+
+// object
+
+console.log(typeof undefined) // undefined
+console.log(typeof null)  // object
+
